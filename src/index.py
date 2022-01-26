60eeec2043a4a1c9704ab6c78a64f1b741492bf3
jpasikainen <asikainen.juhapekka@gmail.com>
2022-01-26 14:27:46 +0200

    Fix import

--- src/index.py
+++ src/index.py
@@ -2,7 +2,7 @@ import geojson
 from flask import Flask
 from flask_restx import Api, Resource
 from flask_cors import CORS
-from .fetch_from_museovirasto import *
+import fetch_from_museovirasto
 
 app = Flask(__name__)
 api = Api(app)
